26b5753b4fd12fd55e331305398cc686c3f672d1
CD1102 <109902973+CD1102@users.noreply.github.com>
2025-07-19 15:43:45 +0100

    Delete VM-Status-Checker.ps1

--- VM-Status-Checker.ps1
+++
@@ -1,4 +0,0 @@
-# reminder to connect to Azure Connect-AzAccount
-$vm = Get-AzVM -name "test-vm" -ResourceGroupName "rg-1" -Status
-$vmStatus = $vm.Statuses[1].DisplayStatus
-Write-Output "VM Status: $vmStatus"
